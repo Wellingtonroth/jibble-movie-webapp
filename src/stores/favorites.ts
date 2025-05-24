2b695f22dc61b21ef3176bdc29f3f322e8063a33
Wellington Roth <wellingtonrothh@gmail.com>
2025-05-24 15:01:08 -0300

    favorites store

--- src/stores/favorites.ts
+++ src/stores/favorites.ts
@@ -1,5 +1,5 @@
-import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { defineStore } from 'pinia'
 
 interface Movie {
   Title: string
@@ -10,39 +10,31 @@ interface Movie {
 export const useFavoritesStore = defineStore('favorites', () => {
   const favorites = ref<Movie[]>([])
 
-  const loadFavorites = () => {
-    const storedFavorites = localStorage.getItem('favoriteMovies')
-    if (storedFavorites) {
-      favorites.value = JSON.parse(storedFavorites)
-    }
-  }
-
   const toggleFavorite = (movie: Movie) => {
     const isFavorite = favorites.value.some(fav => fav.imdbID === movie.imdbID)
-    
     if (isFavorite) {
       favorites.value = favorites.value.filter(fav => fav.imdbID !== movie.imdbID)
     } else {
       favorites.value.push(movie)
     }
-    
     localStorage.setItem('favoriteMovies', JSON.stringify(favorites.value))
   }
 
-  const removeFavorite = (movieId: string) => {
-    favorites.value = favorites.value.filter(movie => movie.imdbID !== movieId)
-    localStorage.setItem('favoriteMovies', JSON.stringify(favorites.value))
+  const loadFavorites = () => {
+    const stored = localStorage.getItem('favoriteMovies')
+    if (stored) {
+      favorites.value = JSON.parse(stored)
+    }
   }
 
-  const isFavorite = (movieId: string) => {
-    return favorites.value.some(fav => fav.imdbID === movieId)
+  const isFavorite = (id: string) => {
+    return favorites.value.some(movie => movie.imdbID === id)
   }
 
   return {
     favorites,
-    loadFavorites,
     toggleFavorite,
-    removeFavorite,
-    isFavorite
+    loadFavorites,
+    isFavorite,
   }
-}) 
+})
