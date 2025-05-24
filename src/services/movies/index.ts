00d88a1d356c72314c79067cdaf20bef694d21cb
Wellington Roth <wellingtonrothh@gmail.com>
2025-05-24 17:08:05 -0300

    update service

--- src/services/movies/index.ts
+++ src/services/movies/index.ts
@@ -1,26 +1,10 @@
 import api from '../axios';
 import { ENDPOINTS } from '../baseUrl';
-
-export interface Movie {
-  Title: string;
-  Year: number;
-  imdbID: string;
-  Type: string;
-  Poster: string;
-}
-
-export interface MovieResponse {
-  page: number;
-  per_page: number;
-  total: number;
-  total_pages: number;
-  data: Movie[];
-}
+import type { MovieResponse } from '@/types/movie'
 
 export const movieService = {
   getAllMovies(page: number = 1) {
     const uriGet = ENDPOINTS.GET_ALL_MOVIES.replace(':page', page.toString());
-
     return api.get<MovieResponse>(uriGet);
   },
 
@@ -28,7 +12,6 @@ export const movieService = {
     const uriGet = ENDPOINTS.SEARCH_MOVIES
       .replace(':title', title)
       .replace(':page', page.toString());
-
     return api.get<MovieResponse>(uriGet);
   },
 
@@ -36,21 +19,6 @@ export const movieService = {
     const uriGet = ENDPOINTS.GET_MOVIES_BY_YEAR
       .replace(':year', year.toString())
       .replace(':page', page.toString());
-
-    return api.get<MovieResponse>(uriGet);
-  },
-
-  getMoviesByType(type: string, page: number = 1) {
-    const uriGet = ENDPOINTS.GET_MOVIES_BY_TYPE
-      .replace(':type', type)
-      .replace(':page', page.toString());
-
-    return api.get<MovieResponse>(uriGet);
-  },
-
-  getMovieById(id: string) {
-    const uriGet = ENDPOINTS.GET_MOVIE_BY_ID.replace(':id', id);
-    
     return api.get<MovieResponse>(uriGet);
   }
 };
