edf073e7ae27919baba518fd0d964dd26f65619e
Wellington Roth <wellingtonrothh@gmail.com>
2025-05-24 15:00:18 -0300

    add baseUrl

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -1,9 +1,9 @@
 export const BASE_URL = 'https://jsonmock.hackerrank.com/api/movies';
 
 export const ENDPOINTS = {
-  SEARCH_MOVIES: (title: string, page: number) => `${BASE_URL}/search/?Title=${title}&page=${page}`,
-  GET_ALL_MOVIES: (page: number) => `${BASE_URL}/search/?page=${page}`,
-  GET_MOVIES_BY_YEAR: (year: number, page: number) => `${BASE_URL}/search/?Year=${year}&page=${page}`,
-  GET_MOVIES_BY_TYPE: (type: string, page: number) => `${BASE_URL}/search/?Type=${type}&page=${page}`,
-  GET_MOVIE_BY_ID: (id: string) => `${BASE_URL}/search/?imdbID=${id}`
+  SEARCH_MOVIES: `${BASE_URL}/search/?Title=:title&page=:page`,
+  GET_ALL_MOVIES: `${BASE_URL}/search/?page=:page`,
+  GET_MOVIES_BY_YEAR: `${BASE_URL}/search/?Year=:year&page=:page`,
+  GET_MOVIES_BY_TYPE: `${BASE_URL}/search/?Type=:type&page=:page`,
+  GET_MOVIE_BY_ID: `${BASE_URL}/search/?imdbID=:id`
 };
