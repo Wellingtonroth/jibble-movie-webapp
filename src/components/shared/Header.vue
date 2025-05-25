5d7313db785dff186f3860076918b3ba812c78f5
Wellington Roth <wellingtonrothh@gmail.com>
2025-05-25 10:30:00 -0300

    temporary header

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <ul>
+      <li>
+        <router-link to="/">Movies List</router-link>
+      </li>
+      <li>
+        <router-link to="/favorites">Favorites</router-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped lang="scss">
+header {
+  padding: 20px;
+}
+</style>
+
