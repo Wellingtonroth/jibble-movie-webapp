198b5c1c481c62a058b6c43c51fbfeca646462f5
Wellington Roth <wellingtonrothh@gmail.com>
2025-05-25 13:56:34 -0300

    emprty state component

---
+++ src/components/shared/EmptyState.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="empty-state">
+    <Icon 
+      icon="mdi:movie-search" 
+      class="empty-state__icon"
+      width="54"
+      height="54"
+    />
+    <h3 class="empty-state__title">
+      {{ title }}
+    </h3>
+    <p class="empty-state__description">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+const { title, description } = defineProps<{
+  title?: string
+  description?: string
+}>();
+</script>
+
+<style scoped lang="scss">
+.empty-state {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 40px;
+  text-align: center;
+
+  &__icon {
+    color: #666;
+    margin-bottom: 16px;
+  }
+
+  &__title {
+    font-size: 16px;
+    color: #333;
+    margin-bottom: 8px;
+  }
+
+  &__description {
+    font-size: 14px;
+    color: #666;
+  }
+}
+</style> 
